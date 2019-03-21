0b183e8c84722f6dd2e8c8f21fc38348458789bf
Ulf <ulf.rustas@gmail.com>
2019-03-21 23:19:23 +0100

    Initial commit. Still very ugly!

---
+++ app/src/main/java/se/falukropp/WeekNumberAppWidget.kt
@@ -0,0 +1,48 @@
+package se.falukropp
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import java.time.LocalDate
+import java.util.*
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class WeekNumberAppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+
+            val weekNumber = GregorianCalendar(Locale("sv", "SW")).get(Calendar.WEEK_OF_YEAR)
+
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.week_number_app_widget)
+            views.setTextViewText(R.id.weekNumber, weekNumber.toString().padStart(2, '0'))
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
